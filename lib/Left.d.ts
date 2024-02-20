b441a0935e08d6f195ada738091d3c42f17ef64c
hannah <hannahellis4242@gmail.com>
2024-02-20 12:41:27 +0000

    building library

--- lib/Left.d.ts
+++ lib/Left.d.ts
@@ -2,6 +2,9 @@ import Either from "./Either";
 export default class Left<A, B> implements Either<A, B> {
     private data;
     constructor(data: A);
+    teeBoth(fn: (a: A) => void, _: (b: B) => void): Either<A, B>;
+    teeRight(_: (a: B) => void): Either<A, B>;
+    tee(fn: (x: A) => void): Either<A, B>;
     promisify(): Promise<A>;
     then(fn: (x: A) => Either<A, B>): Either<A, B>;
     swap(): Either<B, A>;
